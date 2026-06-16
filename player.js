8075b0ef72e7da7184dbd1dbad156577f7d306d9
Isabela Rezer <isabelalopes.pf022@academico.ifsul.edu.br>
2026-06-16 13:38:52 -0300

    ssssss

--- player.js
+++ player.js
@@ -2,21 +2,21 @@ const {sleep} = require('./utils');
 const {Musica} = require('./musica');
 const {Parte} = require('./parte');
 
-const myHero = new Musica('My Hero', 'Foo Fighters');
+const fromTheStart = new Musica('From the start', 'Laufey');
 
-myHero.addParte(
-    new Parte('Too alarmin\ now to talk about \n Take your pictures down and shake it out Truth or consequence say it aloud Use that evidence race it around')
+fromTheStart.addParte(
+    new Parte('')
     )
 
 async function play(){
     try{
         //
-        console.log(myHero.nome + ' por ' + myHero.artista);
+        console.log(fromTheStart.nome + ' por ' + fromTheStart.artista);
         await sleep(34000);
         //
-        console.log('Parte: ' + myHero.parte[0].tag);
-        console.log(myHero.parte[0].letra);
-        await sleep(myHero.parte[0].tempoEspera);
+        console.log('Parte: ' + fromTheStart.parte[0].tag);
+        console.log(fromTheStart.parte[0].letra);
+        await sleep(fromTheStart.parte[0].tempoEspera);
         //
     } catch (error){
     
